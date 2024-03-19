91e3954e8f42e510fdcfe4dd3ac955fab881a154
leanwrldd <leanwrldd@gmail.com>
2024-03-19 15:12:06 +0300

    ana

--- Scripts/King Pin/KingPinPlayer.cs
+++ Scripts/King Pin/KingPinPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Control : MonoBehaviour
+public class KingPinPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
